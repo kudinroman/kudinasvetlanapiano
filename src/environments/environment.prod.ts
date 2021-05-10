be01fe314aff0154135801c9d55ff7059e888cc3
kudinroman <kudinroman82@gmail.com>
2021-05-10 21:45:28 +0300

    init commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import { Environment } from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: "AIzaSyBkL2I75rGvB34c8iNwvClgiJdNYgPxIcU",
+  fbDbUrl: "https://angular-blog-8f745-default-rtdb.firebaseio.com",
+};
